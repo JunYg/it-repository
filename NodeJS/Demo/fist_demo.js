4d3f6918cadef8d73e6ca5cb16e733c41bd1d869
yang jun <junefory@163.com>
2023-07-28 10:45:18 +0800

    Create fist_demo.js

---
+++ NodeJS/Demo/fist_demo.js
@@ -0,0 +1,15 @@
+#!/user/bin/env node
+
+// process.exit(1)
+
+// const express = require('express')
+
+const ProgressBar = require('progress')
+
+const bar = new ProgressBar(':bar', { total: 10 })
+const timer = setInterval(() => {
+  bar.tick()
+  if (bar.complete) {
+    clearInterval(timer)
+  }
+}, 100)
